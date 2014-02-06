d586eb5ef2abba9657a692f5762bfe15ac955e54
godshawk <godshawk@bitbucket.org>
2014-02-06 15:09:11 -0500

    Removed ExampleMod

---
+++ java/com/luna/ce/CheatingEssentials.java
@@ -0,0 +1,14 @@
+package com.luna.ce;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = CheatingEssentials.MODID, version = CheatingEssentials.VERSION)
+public class CheatingEssentials {
+	public static final String MODID = "cheatingessentials";
+	public static final String VERSION = "1.0";
+	
+	public void init(FMLInitializationEvent event) {
+		
+	}
+}
